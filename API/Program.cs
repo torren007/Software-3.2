49d8c49c60f6469d21de5127bad72bfaf96dc2ab
torren007 <torrenprietoruben@gmail.com>
2026-07-09 20:53:47 -0300

    e

--- API/Program.cs
+++ API/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger(options => { options.RouteTemplate = "/openapi/{documentName}.json"; });
